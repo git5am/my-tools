cefc13daa5731cedde63e11e686e2880172ecd29
5am <90157403+git5am@users.noreply.github.com>
2021-11-03 07:53:18 +0200

    fixed a bug where the price wouldn't hnage

--- vitc-price.py
+++ vitc-price.py
@@ -3,26 +3,23 @@ import json
 import time
 import datetime
 
-# defining vitex api
-url = "https://vitex.vite.net/api/v1/exchange-rate?tokenSymbols=VITC-000"
-# connecting to the api
-response = requests.get(url)
-# printing success
-print("Connection To ViteX API Successful!")
-
-# setting up parsing
-response = requests.get(url)
-data = response.text
-parsed = json.loads(data)
-data = parsed["data"]
-type(data)
-usdRate = data[0]['usdRate']
 i = 0
 
-time.sleep(1)
-# printing VITC value!
 while i <= 10:
-    print("VITC's value in dollars is " + str(usdRate))
+# defining vitex api
+    url = "https://vitex.vite.net/api/v1/exchange-rate?tokenSymbols=VITC-000"
+# connecting to the api
+    response = requests.get(url)
+# defining time
     localtime = time.asctime( time.localtime(time.time()) )
-    print("===Local current time :" + localtime + "===")
-    time.sleep(0.5)
+    print("<=========================================>")
+# setting up parsing
+    response = requests.get(url)
+    data = response.text
+    parsed = json.loads(data)
+    data = parsed["data"]
+    type(data)
+    usdRate = data[0]['usdRate']
+    time.sleep(1)
+    # printing VITC value!
+    print("VITC's price @ " + str(localtime) + " is $" + str(usdRate))
